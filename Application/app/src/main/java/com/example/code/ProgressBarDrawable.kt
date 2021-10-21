7a7248294f178328b93a6536138ce491f937a7b6
Devrath <devrath.dev595@gmail.com>
2021-10-21 06:52:43 +0530

    x

--- Application/app/src/main/java/com/example/code/ProgressBarDrawable.kt
+++ Application/app/src/main/java/com/example/code/ProgressBarDrawable.kt
@@ -14,6 +14,7 @@ class ProgressBarDrawable(sectionsList: List<Section>) : Drawable() {
     private val emptyColor = Color.parseColor("#233952")
     private val separatorColor = Color.parseColor("#FFFFFF")
     private var separators: MutableList<RectF>? = null
+
     override fun onLevelChange(level: Int): Boolean {
         invalidateSelf()
         return true
