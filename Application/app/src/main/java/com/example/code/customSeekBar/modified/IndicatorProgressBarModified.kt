f38d857214143d3c3cbf8602594540dee0ab73f7
Devrath <devrath.dev595@gmail.com>
2021-10-23 14:04:57 +0530

    vv

--- Application/app/src/main/java/com/example/code/customSeekBar/modified/IndicatorProgressBarModified.kt
+++ Application/app/src/main/java/com/example/code/customSeekBar/modified/IndicatorProgressBarModified.kt
@@ -8,30 +8,52 @@ import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatSeekBar
 import androidx.core.content.ContextCompat
 import com.example.code.R
+
+enum class SeekbarElementType { PROGRESS_BAR, PROGRESS_INDICATOR, PROGRESS_DIVIDER }
+
 class IndicatorProgressBarModified : AppCompatSeekBar {
+
+    companion object {
+        const val DIVIDER_WIDTH = 5F
+    }
+
+    // List of points for creating sections
     lateinit var indicatorPositions: List<Float>
 
-    /******************** eee **********************************/
-    private var barColor = ContextCompat.getColor(context, R.color.gb_seek_bar_unplayed)
-    private var indicatorColor = ContextCompat.getColor(context, R.color.screen_background)
-    private var progressColor = ContextCompat.getColor(context, R.color.gb_seek_bar_played)
-    private val paint = Paint()
+    /** ************************ Paint Objects ************************ **/
+    private val paintProgressBar = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.FILL
+        color = ContextCompat.getColor(context, R.color.gb_seek_bar_unplayed)
+    }
+    private val paintProgress = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.FILL
+        color = ContextCompat.getColor(context, R.color.gb_seek_bar_played)
+    }
+    private val paintIndicator = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.FILL
+        color = ContextCompat.getColor(context, R.color.screen_background)
+    }
+    /** ************************ Paint Objects ************************ **/
 
-
+    /** ************************ Constructors ************************ **/
     constructor(context: Context?) : super(context!!)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs) { initilize(attrs) }
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyle: Int)
-            : super(context!!, attrs, defStyle) { initilize(attrs) }
+            : super(context!!, attrs, defStyle)
+    /** ************************ Constructors ************************ **/
 
-
-    private var progress = 0F // From float from 0 to 1
+    private var progress = 0F
         set(state) {
             field = state
             invalidate()
         }
 
-    private fun initilize(attrs: AttributeSet?) {
-        paint.isAntiAlias = true
+    // Use to get the width of the view
+    private fun width(): Float {
+        return measuredWidth.toFloat()
     }
 
     @Synchronized
@@ -41,44 +63,53 @@ class IndicatorProgressBarModified : AppCompatSeekBar {
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        paint.style = Paint.Style.FILL // We will only use FILL for the progress bar's components.
-        drawProgressBar(canvas)
-        //drawProgress(canvas)
-        drawIndicators(canvas)
+        performDraw(canvas,SeekbarElementType.PROGRESS_BAR)
+        //performDraw(canvas,SeekbarElementType.PROGRESS_INDICATOR)
+        performDraw(canvas,SeekbarElementType.PROGRESS_DIVIDER)
     }
 
-    /**
-     * Used to get the measuredWidth from the view as a float to be used in the draw methods.
-     */
-    private fun width(): Float {
-        return measuredWidth.toFloat()
-    }
-
+    /** ************************ Draw progress bar elements ************************ **/
+    // Draw the progress bar
     private fun drawProgressBar(canvas: Canvas) {
-        paint.color = barColor
         //drawCenteredBar(canvas, 0F, width())
-        drawCenteredBar(canvas, 32F, width()-32)
+        drawOnCanvas(canvas, paintProgressBar,32F, width()-32)
     }
 
+    // Draw the progress
     private fun drawProgress(canvas: Canvas) {
-        paint.color = progressColor
-
         val barWidth = (progress) * width()
-        drawCenteredBar(canvas, 0F, barWidth)
+        drawOnCanvas(canvas, paintProgress,0F, barWidth)
     }
 
-    private fun drawIndicators(canvas: Canvas) {
-        paint.color = indicatorColor
+    // Draw the indicator
+    private fun drawProgressDivider(canvas: Canvas) {
         indicatorPositions.forEach {
             val barPositionCenter = it * width()
-            val barPositionLeft = barPositionCenter - 5F
-            val barPositionRight = barPositionCenter + 5F
+            val barPositionLeft = barPositionCenter - DIVIDER_WIDTH
+            val barPositionRight = barPositionCenter + DIVIDER_WIDTH
 
-            drawCenteredBar(canvas, barPositionLeft, barPositionRight)
+            drawOnCanvas(canvas, paintIndicator, barPositionLeft, barPositionRight)
+        }
+    }
+    /** ************************ Draw progress bar elements ************************ **/
+
+
+    private fun performDraw(canvas: Canvas, type: SeekbarElementType) {
+        when(type){
+            SeekbarElementType.PROGRESS_BAR -> drawProgressBar(canvas)
+            SeekbarElementType.PROGRESS_INDICATOR -> drawProgress(canvas)
+            SeekbarElementType.PROGRESS_DIVIDER -> drawProgressDivider(canvas)
         }
     }
 
-    private fun drawCenteredBar(canvas: Canvas, left: Float, right: Float) {
+    /**
+     * This function is used to draw on the canvas
+     * * This function is a common function for
+     * * 1 * Drawing the progress bar
+     * * 2 * Drawing the Indicator
+     * * 3 * Drawing the progress for the progress bar
+     */
+    private fun drawOnCanvas(canvas: Canvas, paint:Paint, left: Float, right: Float) {
         //val barTop = (measuredHeight - barHeight) / 2
         //val barBottom = (measuredHeight + barHeight) / 2
         val barTop = height / 1.8 - minimumHeight / 2
